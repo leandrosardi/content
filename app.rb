76984428d7cfcdbd34b79635e93396824679444f
DEV1\Leandro <leandro.sardi@expandedventure.com>
2022-07-26 11:32:57 -0300

    #17 #18

---
+++ app.rb
@@ -0,0 +1,74 @@
+# default screen
+# TODO: Code Me!
+
+# public screens (signup/landing page)
+# TODO: Code Me!
+
+# public screens (login page)
+# TODO: Code Me!
+
+# internal app screens
+get "/content/seminars", :auth => true, :agent => /(.*)/ do
+    erb :"/extensions/content/views/seminars", :layout => :"/views/layouts/core"
+end
+
+get "/content/seminars/new", :auth => true, :agent => /(.*)/ do
+    erb :"/extensions/content/views/new_seminar", :layout => :"/views/layouts/core"
+end
+
+get "/content/seminars/:sid", :auth => true, :agent => /(.*)/ do
+    erb :"/extensions/content/views/view_seminar", :layout => :"/views/layouts/core"
+end
+
+get "/content/seminars/:sid/edit", :auth => true, :agent => /(.*)/ do
+    erb :"/extensions/content/views/edit_seminar", :layout => :"/views/layouts/core"
+end
+
+get "/content/seminars/:sid/edit/:tid", :auth => true, :agent => /(.*)/ do
+    erb :"/extensions/content/views/edit_section", :layout => :"/views/layouts/core"
+end
+
+# public URLs of seminars
+get "/seminars/pub/:folder/:seminar", :agent => /(.*)/ do
+    erb :"/extensions/content/views/seminar", :layout => :"/views/layouts/public"
+end
+
+# private URLs of seminars, for the loggedin users
+get "/seminars/:folder/:seminar", :auth => true, :agent => /(.*)/ do
+    erb :"/extensions/content/views/public", :layout => :"/views/layouts/core"
+end
+
+# filters
+post "/content/filter_new_seminar", :auth => true do
+    erb :"/extensions/content/views/filter_new_seminar"
+end
+
+post "/content/filter_edit_seminar", :auth => true do
+    erb :"/extensions/content/views/filter_edit_seminar"
+end
+
+post "/content/filter_delete_seminar", :auth => true do
+    erb :"/extensions/content/views/filter_delete_seminar"
+end
+
+post "/content/filter_new_version", :auth => true do
+    erb :"/extensions/content/views/filter_new_version"
+end
+
+post "/content/filter_new_section", :auth => true do
+    erb :"/extensions/content/views/filter_new_section"
+end
+
+post "/content/filter_edit_section", :auth => true do
+    erb :"/extensions/content/views/filter_edit_section"
+end
+
+post "/content/filter_delete_section", :auth => true do
+    erb :"/extensions/content/views/filter_delete_section"
+end
+
+# AJAX 
+# TODO: Code Me!
+
+# API
+# TODO: Code Me!
---
+++ extension.rb
@@ -0,0 +1,47 @@
+BlackStack::Extensions::add ({
+    # descriptive name and descriptor
+    :name => 'Content',
+    :description => 'Get paid for your knowledge. Publish online seminars and courses inside a user interface designed to get readers to purchasing.',
+
+    # setup the url of the repository for installation and updates
+    :repo_url => 'https://github.com/leandrosardi/content',
+    :repo_branch => 'main',
+
+    # define version with format <mayor>.<minor>.<revision>
+    :version => '0.0.1',
+
+    # define the name of the author
+    :author => 'leandrosardi',
+
+    # what is the section to add this extension in either the top-bar, the footer, the dashboard.
+    :services_section => 'Services for Marketers',
+    # show this extension as a service in the top bar?
+    :show_in_top_bar => true,
+    # show this extension as a service in the footer?
+    :show_in_footer => true,
+    # show this extension as a service in the dashboard?
+    :show_in_dashboard => true,
+
+    # what are the screens to add in the leftbar
+    :leftbar_icons => [
+        { :label => 'seminars', :icon => :s, :screen => :seminars, },
+        #{ :label => 'readers', :icon => :'user', :screen => :readers, },
+        #{ :label => 'profits', :icon => :'money', :screen => :'profits', },
+    ],
+=begin 
+    # add a folder to the storage from where user can download the exports.
+    :storage_folders => [
+        { :name => 'signals.exports', },
+    ],
+=end
+    # deployment routines
+    :deployment_routines => [{
+        :name => 'install-gems',
+        :commands => [{ 
+            :command => "
+                get install redcarpet -v 3.5.1
+            ",
+            :sudo => true,
+        }],
+    }],
+})
