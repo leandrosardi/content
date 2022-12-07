0bad1f44435d3ed3b3ff26166ff8eba6264a9e4e
leandrosardi <leandro.sardi@expandedventure.com>
2022-12-07 10:22:48 -0300

    leandrosardi/leads#95

--- extension.rb
+++ extension.rb
@@ -1,6 +1,6 @@
 BlackStack::Extensions::add ({
     # descriptive name and descriptor
-    :name => 'Content',
+    :name => 'content',
     :description => 'Publish online seminars and courses inside a user interface designed to get readers to purchasing.',
 
     # setup the url of the repository for installation and updates
